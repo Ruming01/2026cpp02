4718631aa1ebef9162eb00b20ed1bf723a111eb3
Ruming01 <ubin0616@naver.com>
2026-09-10 15:43:08 +0900

    Receipt Project v0.2

--- week02/week02/company.h
+++ week02/week02/company.h
@@ -1,1 +1,18 @@
-#pragma once
+
+#ifndef COMPANY_H
+#define COMPANY_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Company
+{
+private:
+    string name;
+    string tel;
+public:
+    Company(string name, string tel);
+    ~Company();
+    void print() const;
+};
+#endif

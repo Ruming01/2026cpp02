8d369f23ebbdc4d251e380de4ff479781b8fce02
Ruming01 <ubin0616@naver.com>
2026-09-17 16:13:20 +0900

    week03 : Polymorphism v0.3

---
+++ week03/week03/app02.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Pokemon
+{
+public:
+	//Pokemon() { cout << "Æ÷ÄÏ¸ó »ý¼ºÀÚ" << endl; }
+	~Pokemon() { cout << "Æ÷ÄÏ¸ó °´Ã¼ ¼Ò¸ê" << endl; }
+	virtual void attack() const { cout << "Æ÷ÄÏ¸ó ¸öÅë ¹ÚÄ¡±â" << endl; }
+};
+
+class Pikachu : public Pokemon
+{
+public:
+	//Pikachu() { cout << "ÇÇÄ«Ãò »ý¼ºÀÚ" << endl; }
+	~Pikachu() { cout << "ÇÇÄ«Ãò °´Ã¼ ¼Ò¸ê" << endl; }
+	void attack() const { cout << "ÇÇÄ«Ãò 10¸¸ º¼Æ®" << endl; }
+};
+int main()
+{
+	Pokemon* ptr;
+
+	ptr = new Pokemon();
+	ptr->attack();
+	delete ptr;
+	ptr = nullptr;
+
+	ptr = new Pikachu();
+	ptr->attack();
+	delete ptr;
+	ptr = nullptr;
+	return 0;
+}
